a5157d0c22f77081d7903a3270222b4873b12336
vgoreiko <mbox696@gmail.com>
2019-08-11 23:47:56 +0300

    moved some functionality to react

--- src/components/main-form/main-form.tsx
+++ src/components/main-form/main-form.tsx
@@ -1,14 +1,17 @@
+/*global chrome*/
 import React from "react"
 import TopSection from "../top-section/top-section";
 import ParamsSection from "../params-section/Params-section";
 import FormState from "./form-state.model";
+import {Debuggee, getIsTrackedUrl, handleRequestModification, isOptions, urlPatterns} from "../../utils/intercept";
 
 const initState = {
     enabled: true,
     requestUrl: 'Bla',
     statusCode: 200,
     response: '{}',
-    timeout: 1000
+    timeout: 1000,
+    tabId: 0
 }
 
 export default class MainForm extends React.Component {
@@ -40,8 +43,64 @@ export default class MainForm extends React.Component {
     }
 
     componentDidMount(): void {
-        console.log('DidMount')
-        // chrome bind here
+        this.setState({
+            tabId: parseInt(window.location.search.substring(1))
+        })
+
+        if(chrome && chrome.debugger) {
+            const tabId = this.state.tabId
+            window.addEventListener("load", () => {
+                if(chrome.debugger){
+                    chrome.debugger.sendCommand({tabId}, "Network.enable");
+                    chrome.debugger.sendCommand({tabId}, "Network.setRequestInterception", {patterns: urlPatterns});
+                    chrome.debugger.onEvent.addListener(this.onEvent);
+                    // bindClearClick()
+                }
+            });
+            window.addEventListener("unload", function() {
+                chrome.debugger.detach({tabId:tabId});
+            });
+        }
+    }
+
+    needModification(message: string, params: any, debuggeeId: any) {
+        const isEnabledInterceptor = this.state.enabled
+        const filterUrlValue = this.state.requestUrl
+        const isNeededTab = (this.state.tabId === debuggeeId.tabId);
+        const isTrackedUrl = getIsTrackedUrl(params, filterUrlValue);
+        const isMethodOptions = isOptions(params);
+
+        return (isNeededTab && isEnabledInterceptor && isTrackedUrl && !isMethodOptions)
+    }
+
+    onNetworkRequestIntercepted(message: string, params: any, debuggeeId: Debuggee){
+        const neededRequestModification = this.needModification(message, params, debuggeeId);
+        if(neededRequestModification) {
+            setTimeout(() => {
+                handleRequestModification(params, this.state.tabId, this.state.response, this.state.statusCode)
+            }, this.state.timeout)
+        }
+        else{
+            chrome.debugger.sendCommand(
+                {tabId: this.state.tabId},
+                "Network.continueInterceptedRequest",
+                {interceptionId: params.interceptionId});
+        }
+    }
+
+    onEvent(debuggeeId: Debuggee, message: string, params: any) {
+        const filterUrlValue = this.state.requestUrl;
+        const isTrackedUrl = getIsTrackedUrl(params, filterUrlValue);
+
+        if(message === "Network.requestIntercepted"){
+            this.onNetworkRequestIntercepted(message, params, debuggeeId)
+        }
+        //
+        // if (message === "Network.requestWillBeSent") {
+        //     onNetworkRequestWillBeSent(params)
+        // } else if (message === "Network.responseReceived" && isTrackedUrl) {
+        //     appendResponse(params.requestId, params.response);
+        // }
     }
 
     clearLog = () => {
--- src/utils/intercept.tsx
+++ src/utils/intercept.tsx
@@ -1,71 +1,22 @@
+/*global chrome*/
+
 export interface Debuggee {
     tabId?: number;
     extensionId?: string;
     targetId?: string;
 }
 
-const tabId = parseInt(window.location.search.substring(1));
-
 export const urlPatterns = [{
     urlPattern: '*',
     interceptionStage: 'HeadersReceived'
 }];
 
-window.addEventListener("load", function() {
-    if(chrome.debugger){
-        chrome.debugger.sendCommand({tabId}, "Network.enable");
-        chrome.debugger.sendCommand({tabId}, "Network.setRequestInterception", {patterns: urlPatterns});
-        chrome.debugger.onEvent.addListener(onEvent);
-        // bindClearClick()
-    }
-});
-window.addEventListener("unload", function() {
-    chrome.debugger.detach({tabId:tabId});
-});
-
-function onEvent(debuggeeId: Debuggee, message: string, params: any) {
-    const filterUrlValue = getUrlToInterceptElementValue();
-    const isTrackedUrl = getIsTrackedUrl(params, filterUrlValue);
-
-    if(message === "Network.requestIntercepted"){
-        onNetworkRequestIntercepted(message, params, debuggeeId)
-    }
-
-    if (message === "Network.requestWillBeSent") {
-        onNetworkRequestWillBeSent(params)
-    } else if (message === "Network.responseReceived" && isTrackedUrl) {
-        appendResponse(params.requestId, params.response);
-    }
-}
-
-function onNetworkRequestWillBeSent(params: any){
-    console.log("onNetworkRequestWillBeSent")
-}
-
-function onNetworkRequestIntercepted(message: string, params: any, debuggeeId: Debuggee){
-    const neededRequestModification = needModification(message, params, debuggeeId);
-    if(neededRequestModification) {
-        setTimeout(function(){
-            handleRequestModification(params)
-        }, getTimeoutElementValue())
-    }
-    else{
-        chrome.debugger.sendCommand(
-            {tabId},
-            "Network.continueInterceptedRequest",
-            {interceptionId: params.interceptionId});
-    }
-}
-
-function handleRequestModification(params: any) {
+export function handleRequestModification(params: any, tabId: number, newBody: string, statusCode: number) {
     chrome.debugger.sendCommand(
         {tabId},
         "Network.getResponseBodyForInterception",
         {interceptionId: params.interceptionId},
         function(response){
-            // const bodyData = response.base64Encoded ? atob(response.body) : response.body;
-            const newBody = getResponseElementValue();
-            const statusCode = getStatusCodeElementValue();
             params.responseHeaders.status = statusCode;
 
             const keys = Object.keys(params.responseHeaders);
@@ -83,17 +34,7 @@ function handleRequestModification(params: any) {
         })
 }
 
-function needModification(message: string, params: any, debuggeeId: any) {
-    const isEnabledInterceptor = getEnabledCheckboxValue();
-    const filterUrlValue = getUrlToInterceptElementValue();
-    const isNeededTab = (tabId === debuggeeId.tabId);
-    const isTrackedUrl = getIsTrackedUrl(params, filterUrlValue);
-    const isMethodOptions = isOptions(params);
-
-    return (isNeededTab && isEnabledInterceptor && isTrackedUrl && !isMethodOptions)
-}
-
-function isOptions(params: any) {
+export function isOptions(params: any) {
     const isRequest = params.request;
     const isResponse = params.response;
     if (isRequest) {
@@ -104,7 +45,7 @@ function isOptions(params: any) {
     return false
 }
 
-function getIsTrackedUrl(params: any, filterUrlValue: string) {
+export function getIsTrackedUrl(params: any, filterUrlValue: string) {
     const isRequest = params.request;
     if(isRequest){
         return (filterUrlValue && params.request)
@@ -117,58 +58,5 @@ function getIsTrackedUrl(params: any, filterUrlValue: string) {
     }
 }
 
-function appendResponse(requestId: string, response: any) {
-    // let requestDiv = requests[requestId];
-    // requestDiv.appendChild(formatHeaders(response.requestHeaders));
-    //
-    // let statusLine = document.createElement("div");
-    // statusLine.textContent = "\nHTTP/1.1 " + response.status + " " +
-    //     response.statusText;
-    // requestDiv.appendChild(statusLine);
-    // requestDiv.appendChild(formatHeaders(response.headers));
-    console.log('appendResponse')
-}
 
-// function formatHeaders(headers: any) {
-//     let text = "";
-//     for (let name in headers)
-//         text += name + ": " + headers[name] + "\n";
-//     const div = document.createElement("div");
-//     div.textContent = text;
-//     return div;
-// }
-
-// function parseURL(url: string) {
-//     const result = any;
-//     const match = url.match(
-//         /^([^:]+):\/\/([^\/:]*)(?::([\d]+))?(?:(\/[^#]*)(?:#(.*))?)?$/i);
-//     if (!match)
-//         return result;
-//     result.scheme = match[1].toLowerCase();
-//     result.host = match[2];
-//     result.port = match[3];
-//     result.path = match[4] || "/";
-//     result.fragment = match[5];
-//     return result;
-// }
-
-function getEnabledCheckboxValue() {
-    return false
-}
-
-function getUrlToInterceptElementValue() {
-    return ''
-}
-
-function getStatusCodeElementValue() {
-    return 200
-}
-
-function getResponseElementValue() {
-    return ''
-}
-
-function getTimeoutElementValue() {
-    return 0
-}
 
